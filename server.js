798f1ef1903fec25b5fe0a35e53dd62d35d1b8c4
Shanb98 <shanbasnayake@hotmail.com>
2024-02-07 18:56:16 +0530

    admin

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const dotenv = require("dotenv"); // Import the 'dotenv' library
 const connectiondb = require("./config/dbConnection");
-
+const dotenv = require("dotenv"); // Import the 'dotenv' library
+const errorHandler = require("./middleware/errorHandler");
 
 dotenv.config();
 connectiondb();
@@ -9,6 +9,12 @@ connectiondb();
 
 const app = express();
 const port = process.env.PORT || 5000
+
+
+
+app.use(express.json());  //this an inbulid middleware -> this provide a passer which will pass the data that we recive from the client on the server
+app.use("/api/admins", require("./routes/adminRoutes"));
+app.use(errorHandler);
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
   });
