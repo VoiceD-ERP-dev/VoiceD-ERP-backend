c78e949a36172534a07bbe53dd5451cffb168fd2
dasuntheekshanadev <dasuntheekshana12@gmail.com>
2024-02-07 14:25:08 +0530

    test2

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000
 app.listen(port, () => {
-    console.log(`Server running on portt ${port}`);
+    console.log(`Server running on port ${port}`);
   });
   
