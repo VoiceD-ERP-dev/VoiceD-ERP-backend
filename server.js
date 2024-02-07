947e3e21875f38fc6ac81d024db1d6d41e001f5b
Shanb98 <shanbasnayake@hotmail.com>
2024-02-07 15:08:13 +0530

    connect DB

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv"); // Import the 'dotenv' library
+const connectiondb = require("./config/dbConnection");
 
 
 dotenv.config();
+connectiondb();
+
+
 const app = express();
 const port = process.env.PORT || 5000
 app.listen(port, () => {
