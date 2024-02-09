2bb708147fac1b23708526260c7e2e5a2ed65638
Shan Basnayake <shanbasnayake@hotmail.com>
2024-02-09 15:35:09 +0530

    change

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const {registerUser,getUser} = require('../controllers/userControllers')
-const router = express.Router()
-
-router.post("/register",registerUser);
-router.get("/:nic",getUser);
-
-module.exports = router;
