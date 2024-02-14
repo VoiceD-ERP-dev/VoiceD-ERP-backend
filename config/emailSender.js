440d2f52cea85f5788a738e24cb1d7892f9addc7
dasuntheekshanadev <dasuntheekshana12@gmail.com>
2024-02-14 10:28:31 +0530

    added pdf sender and email sending process

---
+++ config/emailSender.js
@@ -0,0 +1,45 @@
+const nodemailer = require('nodemailer');
+
+const config = {
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: true,
+    auth: {
+        user: "dasuntheekshana12@gmail.com",
+        pass: "yszs kvfj dbjq jfqo"
+    },
+};
+
+const transporter = nodemailer.createTransport(config);
+
+const sendMail = (data) => {
+    return new Promise((resolve, reject)=>{
+        // Handle attachment as Buffer
+        const attachments = [];
+        if (data.attachment) {
+            attachments.push({
+                filename: data.attachment.filename,
+                content: data.attachment.content
+            });
+        }
+
+        const mailOptions = {
+            from: data.from,
+            to: data.to,
+            subject: data.subject,
+            text: data.text,
+            attachments: attachments
+        };
+
+        transporter.sendMail(mailOptions, (err, info) => {
+            if(err){
+                reject(err);
+            } else {
+                resolve(info.response);
+            }
+        });
+    });
+};
+
+module.exports = sendMail;
--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,22 +1,24 @@
 const asyncHandler = require("express-async-handler");
-const  Customer = require("../models/customerModel");
+const Customer = require("../models/customerModel");
 const Order = require("../models/orderModel");
 const Invoice = require("../models/invoiceModel");
+const sendMail = require("../config/emailSender");
+const sendPdfEmail = require('../config/pdfGenerator');
+
 //@desc Get all customers
 //@route GET /api/customers
 //@access private
-const getCustomers = asyncHandler(async (req, res) => {    //async makes a function return a Promise
-  //getting the customers from the db
-  //getting all the customers created by the login in admin
-  const customers = await Customer.find({ user_id: req.admin.id });  //await makes a function wait for a Promise
-    res.status(200).json(customers);
-  });
+const getCustomers = asyncHandler(async (req, res) => {
+  const customers = await Customer.find({ user_id: req.admin.id });
+  res.status(200).json(customers);
+});
 
 //@desc Post customer by id
 //@routs POST /api/customers/
 //@access private
 const createCustomer = asyncHandler(async (req, res) => {
   const { name, email, phone, orders, invoice } = req.body;
+
 
   // Validate customer data
   if (!name || !email || !phone || !Array.isArray(orders) || !invoice || !invoice.description) {
@@ -49,7 +51,7 @@ const createCustomer = asyncHandler(async (req, res) => {
     // Create invoice for the customer
     const invoiceObj = await Invoice.create({
       customer: customer._id,
-      order: orderIds, // Assuming all orders are associated with this invoice
+      order: orderIds,
       description: invoice.description,
     });
 
@@ -58,74 +60,75 @@ const createCustomer = asyncHandler(async (req, res) => {
     customer.invoice = invoiceObj._id;
     await customer.save();
 
+    const pdfSent = await sendPdfEmail(name,email,phone);
+    if(!pdfSent){
+      throw new Error("Failed to send PDF via email!!")
+    }
+
     res.status(201).json(customer);
   } catch (error) {
+    console.error("Error creating customer:", error);
     res.status(500).json({ message: "Server error" });
   }
 });
-module.exports = { createCustomer };
+
 //@desc Get new customers
 //@route GET /api/customers/:id
 //@access private
 const getCustomer = asyncHandler(async (req, res) => {
-    //getting the customer by id
-    const customer = await Customer.findById(req.params.id);
-    if(!customer) {
-      res.status(404);
-      throw new Error("Customer not found");
-    }
+  const customer = await Customer.findById(req.params.id);
+  if (!customer) {
+    res.status(404);
+    throw new Error("Customer not found");
+  }
 
-    res.status(200).json(customer);
-  });
+  res.status(200).json(customer);
+});
+
 //@desc Update all customers
 //@route UPDATE /api/customers/:id
 //@access private
 const updateCustomer = asyncHandler(async (req, res) => {
-    const customer = await Customer.findById(req.params.id);
-    if(!customer) {
-      res.status(404);
-      throw new Error("Customer not found");
-    }
-    //checking weather the admin id of the login admin is matching with the updating customer
-    if (customer.user_id.toString() !== req.admin.id) {
-      res.status(402);
-      throw new Error("User don't have permission to update this");
-    }
-    const updatedCustomer = await Customer.findByIdAndUpdate(
-      req.params.id, //getting the id of the customer that needs to be updated
-      req.body, //getting the updated body
-      { new: true } //query option
-    );
-  
-  
-    res.status(200).json(updatedCustomer);
-  });
+  const customer = await Customer.findById(req.params.id);
+  if (!customer) {
+    res.status(404);
+    throw new Error("Customer not found");
+  }
+  if (customer.user_id.toString() !== req.admin.id) {
+    res.status(402);
+    throw new Error("User don't have permission to update this");
+  }
+  const updatedCustomer = await Customer.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
 
+  res.status(200).json(updatedCustomer);
+});
 
 //@desc Delete a customer
 //@routs DELETE /api/customers/:id
 //@access private
 const deleteCustomer = asyncHandler(async (req, res) => {
-    const customer = await Customer.findById(req.params.id);
-    if(!customer) {
-      res.status(404);
-      throw new Error("Customer not found");
-    }
-    //checking weather the admin id of the login admin is matching with the deleting customer
-    if (customer.user_id.toString() !== req.admin.id) {
-      res.status(402);
-      throw new Error("User don't have permission to update this");
-    }
+  const customer = await Customer.findById(req.params.id);
+  if (!customer) {
+    res.status(404);
+    throw new Error("Customer not found");
+  }
+  if (customer.user_id.toString() !== req.admin.id) {
+    res.status(402);
+    throw new Error("User don't have permission to update this");
+  }
 
-    await Customer.deleteOne({ _id: req.params.id });
-    res.status(200).json(customer); 
-  });
+  await Customer.deleteOne({ _id: req.params.id });
+  res.status(200).json(customer);
+});
 
 module.exports = {
-    getCustomers,
-    createCustomer,
-    getCustomer,
-    updateCustomer,
-    deleteCustomer,
-    
-  };
+  getCustomers,
+  createCustomer,
+  getCustomer,
+  updateCustomer,
+  deleteCustomer,
+};
