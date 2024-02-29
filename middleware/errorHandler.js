c413b4e5536b130043119d73af1a1487899d0e2d
dasuntheekshanadev <dasuntheekshana12@gmail.com>
2024-02-29 10:26:53 +0530

    Refactored the ErrorHandler and Added encoding txt files to the invoice template

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -8,35 +8,39 @@ const errorHandler = (err, req, res, next) => {
             message: err.message,
             stackTrace: err.stack,
           });
-          break;
+          return; // Add return statement here
         case constants.NOT_FOUND:
           res.json({
             title: "Not Found",
             message: err.message,
             stackTrace: err.stack,
           });
+          return; // Add return statement here
         case constants.UNAUTHORIZED:
           res.json({
             title: "Unauthorized",
             message: err.message,
             stackTrace: err.stack,
           });
+          return; // Add return statement here
         case constants.FORBIDDEN:
           res.json({
             title: "Forbidden",
             message: err.message,
             stackTrace: err.stack,
           });
+          return; // Add return statement here
         case constants.SERVER_ERROR:
           res.json({
             title: "Server Error",
             message: err.message,
             stackTrace: err.stack,
           });
+          return; // Add return statement here
         default:
           console.log("No Error, All good !");
           break;
-      }
+    }
 };
 
 module.exports = errorHandler;
