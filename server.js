32d8b9ffa4d60dfd9a3bf0b7f3b29f813e6cd5e5
Shan Basnayake <shanbasnayake@hotmail.com>
2024-02-29 15:32:14 +0530

    change cors

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors({ origin: "https://voiced-erp-backend.onrender.com" }));
+app.use(cors({
+    origin: 'https://voice-d-erp-frontend.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // enable credentials (if needed)
+  }));
 app.options("*", cors());
 app.use("/api/admins", require("./routes/adminRoutes"));
 app.use("/api/customers", require("./routes/customerRoutes"));
